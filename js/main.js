d8e69296f9dc6523bd03c9a9d5a8cbf70fd50e71
modusol <aperhyul@naver.com>
2023-02-17 12:05:50 +0900

    1

--- js/main.js
+++ js/main.js
@@ -55,6 +55,24 @@
             '<i class="bi bi-arrow-right"></i>'
         ],
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
     
 })(jQuery);
 
